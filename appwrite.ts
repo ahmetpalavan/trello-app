f3df8a227831e3062393bc8a4237b9bce4b95f6e
ahmetpalavan <ahmetpalavann@gmail.com>
2023-06-12 16:50:09 +0300

    added appwrite

---
+++ appwrite.ts
@@ -0,0 +1,11 @@
+import { Client, Account, ID, Databases, Storage } from "appwrite";
+
+const client = new Client();
+
+client.setEndpoint("https://cloud.appwrite.io/v1").setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID || "");
+
+const account = new Account(client);
+const database = new Databases(client);
+const storage = new Storage(client);
+
+export { client, account, database, storage, ID };
