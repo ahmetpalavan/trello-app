ca11f3f735ae72a08e953f806390605df4cb5786
ahmetpalavan <ahmetpalavann@gmail.com>
2023-06-24 16:34:28 +0300

    change in appwrite

--- appwrite.ts
+++ appwrite.ts
@@ -1,11 +1,9 @@
 import { Client, Account, ID, Databases, Storage } from "appwrite";
 
-const client = new Client();
-
-client.setEndpoint("https://cloud.appwrite.io/v1").setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID || "");
+const client = new Client().setEndpoint('https://cloud.appwrite.io/v1').setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!);
 
 const account = new Account(client);
-const database = new Databases(client);
+const databases = new Databases(client);
 const storage = new Storage(client);
 
-export { client, account, database, storage, ID };
+export { client, account, databases, storage, ID };
